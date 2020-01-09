659993c4995155d2d12a6402ee60f4f362d89f68
Maxim Khlamov <mkhlamov@gmail.com>
2020-01-09 18:31:46 +0300

    Obstacles sounds and material
    Add obstacles prefabs.
    Add sound after passing and hitting obstacles.
    Add material change after passing obstacles.

--- SkyRollerClone/Assets/Scripts/Blocks/Obstacle.cs
+++ SkyRollerClone/Assets/Scripts/Blocks/Obstacle.cs
@@ -4,14 +4,30 @@ using UnityEngine;
 
 namespace SkyRollerClone
 {
+    [RequireComponent(typeof(AudioSource))]
     public class Obstacle : MonoBehaviour
     {
+        private AudioSource _audioSource;
+
+        #region Monobehaviour
+        private void Start()
+        {
+            Init();
+        }
+
         private void OnTriggerEnter(Collider other)
         {
             if (other.CompareTag("Player"))
             {
+                _audioSource.Play();
                 GameManager.Instance.GameLost();
             }
+        }
+        #endregion
+
+        private void Init()
+        {
+            _audioSource = GetComponent<AudioSource>();
         }
     }
 }
---
+++ SkyRollerClone/Assets/Scripts/Blocks/PassedObstaclesHandler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SkyRollerClone
+{
+    [RequireComponent(typeof(BoxCollider))]
+    [RequireComponent(typeof(AudioSource))]
+    public class PassedObstaclesHandler : MonoBehaviour
+    {
+        [SerializeField]
+        private Material _defaultMaterial;
+        [SerializeField]
+        private Material _passedMaterial;
+        private List<Renderer> _renderers;
+        private AudioSource _audioSource;
+
+        #region Monobehaviour
+        private void Start()
+        {
+            Init();
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                foreach (Renderer r in _renderers)
+                {
+                    r.material = _passedMaterial;
+                }
+                _audioSource.Play();
+            }
+        }
+        #endregion
+
+        private void Init()
+        {
+            _renderers = new List<Renderer>();
+            _audioSource = GetComponent<AudioSource>();
+            foreach (Transform t in transform)
+            {
+                Renderer r = t.gameObject.GetComponent<Renderer>();
+                if (r != null)
+                {
+                    _renderers.Add(r);
+                    r.material = _defaultMaterial;
+                }
+            }
+        }
+    }
+}
